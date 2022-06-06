df644f18a3af195517f5c534983db3ca57b98d43
gray jimenez <coreyjimenez1@gmail.com>
2022-06-06 15:35:34 -0600

    trying to fix /jobs when showing applied jobs in list

--- src/JobList.js
+++ src/JobList.js
@@ -5,6 +5,7 @@ import "./JobList.css";
 const JobList = ({ currentUser }) => {
   let parsedJobs = JSON.parse(localStorage.getItem("_jobsApplied"));
   let userD = JSON.parse(localStorage.getItem("_userD"));
+  let toke = localStorage.getItem("_token");
   const [jobsApplied, setJobsApplied] = useState(parsedJobs);
   console.log(userD);
   const [jobs, setJobs] = useState([{}]);
@@ -53,7 +54,7 @@ const JobList = ({ currentUser }) => {
               <td className="JobList-table-body-link">
                 <a href={`/jobs/${job.id}`}>View Job</a>
               </td>
-              {!jobsApplied ? (
+              {!token ? null : !jobsApplied ? (
                 checkIfAppliedToJob(job.id) &&
                 job.id &&
                 currentUser.applications ? (
